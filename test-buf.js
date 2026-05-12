156eaf1c87701b7a261227eb1c5b898a83d93667
bhanuprakash2002 <bhanuprakashreddy.keesara@gmail.com>
2026-05-12 20:04:20 +0530

    chore: remove test scripts from repo

--- test-buf.js
+++
@@ -1,4 +0,0 @@
-const orig = Buffer.from("hello world");
-const b = Buffer.from(orig, "base64");
-console.log(b.toString());
-console.log(orig.equals(b));
--- test-mutate.js
+++
@@ -1,4 +0,0 @@
-const { Translate } = require('@google-cloud/translate').v2;
-const clientConfig = { projectId: 'test' };
-const gTranslate = new Translate(clientConfig);
-console.log(clientConfig);
